23c3ef8c8c530f9335062ceb52f47bff17a31756
Dugyu <yxylyzy@126.com>
2019-01-30 19:45:11 -0500

    reorganize

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
-from flask_session import Session
-from tempfile import mkdtemp
- 
-app = Flask(__name__)
- 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
- 
-@app.route("/")
-def index():
-    return render_template("map.html")
+from starsapp import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
