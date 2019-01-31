b483b0f668b342d852324ef9fb65fa2605099d4a
Dugyu <yxylyzy@126.com>
2019-01-30 19:20:05 -0500

    fix locations

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+ 
+app = Flask(__name__)
+ 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+ 
+@app.route("/")
+def index():
+    return render_template("map.html")
